0e06511e20894e8cf1dc503c6854380d4e6f5dc1
Morteza Rostami <morteza.rostami.55b@gmail.com>
2023-11-15 22:07:42 +0400

    learning files

--- src/source/files.js
+++ src/source/files.js
@@ -1,18 +1,40 @@
 import os from 'os'
 import path from 'path'
 import { fileURLToPath } from 'url'
-
+import fs from 'fs'
 
 function leason1() {
 
-  console.log('learn about node js working with files!')
-
+  const __filename = fileURLToPath(import.meta.url)
+  const __dirname = path.dirname(__filename)
+  const filePath = path.join(__dirname, 'files', 'just.txt')
+  
   // file system
 
-  // write file
+  // write file (async)
+  //fs.writeFile('./src/')
   
-  
-  // read file
+  // read file (async)
+
+  // path from ./root directory
+  fs.readFile(
+    filePath, 
+    {encoding: 'utf8'},
+    (err, data) => {
+    if (err) throw err
+    console.log(data)
+  })
+  /* 
+  by default returns a buffer
+  <Buffer 74 68 69 73 20 69 73 20 61 20 66 69 6c 65 20 62 79 20 6d 6f 72 74 65 7a 61 20 72 6f 73 74 61 6d 69 2e>
+  */
+
+  // listen for an exception (event)
+  process.on('uncaughtException', err => {
+    console.error(`there was an uncaught error: ${err}`)
+    // exiting the process (program)
+    process.exit(1)
+  })
   
 }
 
